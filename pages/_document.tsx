18a5c842b1d7eb5bf6518d4a01ab1d0b8b56eab1
rbgksqkr <rbgks1937@naver.com>
2023-07-31 23:31:51 +0900

    [FEAT] lighthouse pwa 검사 완료

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
+        <meta name='theme-color' content='#3D39F1' />
         <link rel='manifest' href='/manifest.json' />
       </Head>
       <body>

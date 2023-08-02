69864611ca6d6bd1f78a389b672da81d06433fcb
rbgksqkr <rbgks1937@naver.com>
2023-08-02 15:51:44 +0900

    Design: #6 - 전체 컴포넌트 공통 레이아웃으로 감싸기

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import Head from 'next/head';
 import GlobalStyle from '../src/styles/globalStyle';
 import React from 'react';
 import { Wrapper } from '@googlemaps/react-wrapper';
+import styled from 'styled-components';
 
 const Reactproject: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
@@ -14,10 +15,19 @@ const Reactproject: NextPage<AppProps> = ({ Component, pageProps }: AppProps) =>
       </Head>
       <GlobalStyle />
       <Wrapper apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_KEY!}>
-        <Component {...pageProps} />
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
       </Wrapper>
     </>
   );
 };
+
+const MainLayout = styled.div`
+  width: 390px;
+  height: 844px;
+  border: 2px solid black;
+  margin: 0 auto;
+`;
 
 export default Reactproject;

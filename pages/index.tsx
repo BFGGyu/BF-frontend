61799d92f9655e2ca993581946ebb61deb30a05b
rbgksqkr <rbgks1937@naver.com>
2023-08-02 16:05:57 +0900

    feat: #6 - 헤더와 바디 레이아웃 구분

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,12 @@ import styled from 'styled-components';
 const Index: NextPage = () => {
   return (
     <>
-      <ASD>지도 페이지 abcde</ASD>
+      <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+        <div style={{ flexBasis: '30%', border: '3px solid black' }}>header</div>
+        <div style={{ flexBasis: '70%', border: '3px solid black' }}>body</div>
+      </div>
     </>
   );
 };
 
-const ASD = styled.div`
-  font-family: 'cursive';
-  fontsize: 20;
-`;
 export default Index;

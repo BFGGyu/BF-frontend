a539834e46d79903f4b386b87005c793b9887577
rbgksqkr <rbgks1937@naver.com>
2023-08-02 15:50:23 +0900

    Feat: #6 - 서버 사이드 렌더링 시 styled-components가 헤더에 주입 되도록 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,49 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps
+} from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang='en'>
-      <Head>
-        <meta name='theme-color' content='#3D39F1' />
-        <link rel='manifest' href='/manifest.json' />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const originalRenderPage = ctx.renderPage;
+
+    // Run the React rendering logic synchronously
+    ctx.renderPage = () =>
+      originalRenderPage({
+        // Useful for wrapping the whole react tree
+        enhanceApp: (App) => App,
+        // Useful for wrapping in a per-page basis
+        enhanceComponent: (Component) => Component
+      });
+
+    // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html lang='en'>
+        <Head>
+          <meta name='theme-color' content='#3D39F1' />
+          <link rel='manifest' href='/manifest.json' />
+          <link
+            href='https://cdn.jsdelivr.net/gh/sunn-us/SUIT/fonts/variable/woff2/SUIT-Variable.css'
+            rel='stylesheet'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;

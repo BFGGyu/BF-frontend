b7ac5920a7de345f6bc0f2bc824b479ef0bf78bf
rbgksqkr <rbgks1937@naver.com>
2023-08-02 13:59:08 +0900

    Design: #3 - font constant 추가

---
+++ src/constants/fonts.ts
@@ -0,0 +1,36 @@
+import { CSSProperties } from 'react';
+
+const HEADLINE1: CSSProperties = {
+  fontSize: '24px',
+  fontWeight: '700'
+};
+
+const HEADLINE2: CSSProperties = {
+  fontSize: '16px',
+  fontWeight: '700'
+};
+
+const BODY1: CSSProperties = {
+  fontSize: '16px',
+  fontWeight: '500'
+};
+
+const BODY2: CSSProperties = {
+  fontSize: '14px',
+  fontWeight: '500'
+};
+
+const CAPTION: CSSProperties = {
+  fontSize: '12px',
+  fontWeight: '500'
+};
+
+const FONT = {
+  HEADLINE1,
+  HEADLINE2,
+  BODY1,
+  BODY2,
+  CAPTION
+};
+
+export default FONT;

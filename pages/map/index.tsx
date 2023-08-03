2ab362061b510d6065c2a901005393a18967b956
rbgksqkr <rbgks1937@naver.com>
2023-08-03 12:48:11 +0900

    Feat: #6 - TMAP 지도에 마커찍기

--- pages/map/index.tsx
+++ pages/map/index.tsx
@@ -10,24 +10,11 @@ declare global {
   }
 }
 
-function initTmap() {
-  // map 생성
-  // Tmapv2.Map을 이용하여, 지도가 들어갈 div, 넓이, 높이를 설정합니다.
-  const map = new Tmapv2.Map('map_div', {
-    center: new Tmapv2.LatLng(37.566481622437934, 126.98502302169841), // 지도 초기 좌표
-    width: '890px',
-    height: '400px',
-    zoom: 15
-  });
-}
-
 const Index: NextPage = () => {
-  // const [map, setMap] = useState(null);
-  // const mapRef = useRef<HTMLDivElement | null>(null);
-  // const [currentCoords, setCurrentCoords] = useState({ lat: 0, lng: 0 });
-  // const [isLoading, setIsLoading] = useState<boolean>(true);
+  const mapRef = useRef<HTMLDivElement | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  // // 현재 위치(위도, 경도) 가져온다.
+  // 현재 위치(위도, 경도) 가져온다.
   // const getPosSuccess = async (pos: GeolocationPosition) => {
   //   setCurrentCoords({
   //     lat: pos.coords.latitude,
@@ -54,26 +41,61 @@ const Index: NextPage = () => {
   //   getCurrentPosition();
   // }, []);
 
-  // useEffect(() => {
-  //   const newMap = new window.google.maps.Map(mapRef.current, {
-  //     center: { lat: currentCoords.lat, lng: currentCoords.lng },
-  //     zoom: 16
-  //   });
-  //   setMap(newMap);
-  //   console.log('무한렌더링 방지: ', currentCoords.lat);
-  // }, [currentCoords]);
+  const initTmap = () => {
+    // map 생성
+    // Tmapv2.Map을 이용하여, 지도가 들어갈 div, 넓이, 높이를 설정합니다.
+    const map = new window.Tmapv2.Map('map_div', {
+      center: new window.Tmapv2.LatLng(37.53084364186228, 127.078908811749), // 지도 초기 좌표
+      width: '100%',
+      height: '400px',
+      zoom: 12
+    });
 
-  const handleClick = () => {
-    initTmap();
+    // 출발
+    const startMarker = new window.Tmapv2.Marker({
+      position: new window.Tmapv2.LatLng(37.519892712436906, 127.02810900563199),
+
+      icon: 'http://tmapapi.sktelecom.com/upload/tmap/marker/pin_r_m_s.png',
+      iconSize: new window.Tmapv2.Size(24, 38),
+      map: map
+    });
+
+    // 도착
+    const endMarker = new window.Tmapv2.Marker({
+      position: new window.Tmapv2.LatLng(37.49288934463672, 127.11971717230388),
+
+      icon: 'http://tmapapi.sktelecom.com/upload/tmap/marker/pin_r_m_e.png',
+      iconSize: new window.Tmapv2.Size(24, 38),
+      map: map
+    });
+
+    // 경로1
+    const pathMarker1 = new window.Tmapv2.Marker({
+      position: new window.Tmapv2.LatLng(37.5591696189164, 127.07389565460413),
+      icon: 'http://tmapapi.sktelecom.com/upload/tmap/marker/pin_b_m_p.png',
+      iconSize: new window.Tmapv2.Size(24, 38),
+      map: map
+    });
+
+    // 경로2
+    const pathMarker2 = new window.Tmapv2.Marker({
+      position: new window.Tmapv2.LatLng(37.52127761904626, 127.13346617572014),
+      icon: 'http://tmapapi.sktelecom.com/upload/tmap/marker/pin_b_m_p.png',
+      iconSize: new window.Tmapv2.Size(24, 38),
+      map: map
+    });
+
+    setIsLoading(false);
   };
+
+  useEffect(() => {
+    initTmap();
+  }, []);
 
   return (
     <>
-      <h1>지도 페이지</h1>
-      {/* {isLoading && <Loading />} */}
-      {/* <div ref={mapRef} id='map' style={{ display: 'none', width: '100%', height: '100%' }}></div> */}
-      <div onClick={handleClick}>지도 생성</div>
-      <div id='map_div'></div>
+      {isLoading && <Loading />}
+      <div ref={mapRef} id='map_div'></div>
     </>
   );
 };

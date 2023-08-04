9dfb1de2322a14d17e2aedfc9c49802251fe197a
rbgksqkr <rbgks1937@naver.com>
2023-08-04 14:44:47 +0900

    Feat: #8 - 추천 박물관 컴포넌트 구현

--- src/components/main/FooterSection.tsx
+++ src/components/main/FooterSection.tsx
@@ -1,11 +1,26 @@
+import COLOR from '@/constants/colors';
+import FONT from '@/constants/fonts';
+import Image from 'next/image';
 import styled from 'styled-components';
 
 const FooterSection = () => {
   return (
     <FooterWrapper>
       <PlaceWrapper>
-        <PlaceItem></PlaceItem>
-        <PlaceItem></PlaceItem>
+        <PlaceItem>
+          <IconWrapper>
+            <Image src='/images/wheelChair.svg' width={30} height={30} />
+            <Image src='/images/elevator.svg' width={30} height={30} />
+            <Image src='/images/slope.svg' width={30} height={30} />
+          </IconWrapper>
+          <TextWrapper>
+            <PlaceName style={FONT.HEADLINE2}>국립고궁 박물관</PlaceName>
+            <PlaceType style={FONT.BODY2}>박물관</PlaceType>
+            <PlaceLocation style={FONT.BODY2}>서울 종로구 세종로</PlaceLocation>
+          </TextWrapper>
+
+          <RouteButton style={FONT.HEADLINE2}>길찾기</RouteButton>
+        </PlaceItem>
       </PlaceWrapper>
     </FooterWrapper>
   );
@@ -13,7 +28,7 @@ const FooterSection = () => {
 
 const FooterWrapper = styled.div`
   background-color: green;
-  height: 30vh;
+  height: 40vh;
   width: 100%;
   display: flex;
   justify-content: center;
@@ -35,6 +50,41 @@ const PlaceItem = styled.div`
   width: 45%;
   border: 3px solid black;
   border-radius: 16px;
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+  gap: 10px;
 `;
 
+const IconWrapper = styled.div`
+  display: flex;
+  flexbasis: 20%;
+  gap: 5px;
+`;
+
+const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-basis: 50%;
+  gap: 5px;
+`;
+
+const PlaceName = styled.div``;
+
+const PlaceType = styled.div`
+  color: ${COLOR.ORANGE};
+`;
+
+const PlaceLocation = styled.div``;
+
+const RouteButton = styled.div`
+  background-color: ${COLOR.BLUE1};
+  color: white;
+  padding: 10px;
+  width: 60%;
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 export default FooterSection;

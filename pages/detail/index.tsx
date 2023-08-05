4393868cc361bfb278617524e4448f90aba5c5a9
rbgksqkr <rbgks1937@naver.com>
2023-08-05 17:24:12 +0900

    Feat: #23 - 리뷰 컴포넌트 구현

--- pages/detail/index.tsx
+++ pages/detail/index.tsx
@@ -15,6 +15,18 @@ const DetailPage = () => {
     startTimeAt: '10:00'
   });
 
+  const [reviewList, setReviewList] = useState([
+    { id: 0, starRate: 5, count: 4, text: '편안하고 안전한 길이였어요.' },
+    { id: 1, starRate: 1, count: 3, text: '불편한 길이였어요.' },
+    { id: 2, starRate: 4, count: 2, text: '좋은데요?' },
+    {
+      id: 3,
+      starRate: 5,
+      count: 1,
+      text: '편안하고 안전한 길이였어요. 근데 만약에 텍스트가 길어지면 자를지 그냥 보여줄지?'
+    }
+  ]);
+
   return (
     <DetailWrapper>
       <HeaderWrapper>
@@ -22,7 +34,7 @@ const DetailPage = () => {
         <div style={FONT.BODY1}>고궁 박물관</div>
       </HeaderWrapper>
       <ImageSection></ImageSection>
-      <div style={{ display: 'flex' }}>
+      <PlaceInfomation>
         <LeftWrapper>
           <InfoSection place={selectedPlace} />
         </LeftWrapper>
@@ -31,7 +43,22 @@ const DetailPage = () => {
             길찾기
           </Button>
         </RightWrapper>
-      </div>
+      </PlaceInfomation>
+      <ReviewSection>
+        <ReviewHeader style={FONT.HEADLINE2}>방문자 리뷰 49 명</ReviewHeader>
+        {reviewList.map((review) => (
+          <ReviewBody>
+            <ReviewScore style={FONT.BODY2}>
+              <StarRating>
+                {'⭐️'.repeat(review.starRate)}
+                {'🍎'.repeat(5 - review.starRate)}
+              </StarRating>
+              <ReviewCount>{review.count}번째 방문자</ReviewCount>
+            </ReviewScore>
+            <ReviewText style={FONT.BODY2}>{review.text}</ReviewText>
+          </ReviewBody>
+        ))}
+      </ReviewSection>
     </DetailWrapper>
   );
 };
@@ -57,6 +84,10 @@ const ImageSection = styled.div`
   background-color: ${COLOR.GREY};
 `;
 
+const PlaceInfomation = styled.div`
+  display: flex;
+`;
+
 const LeftWrapper = styled.div`
   display: flex;
   flex-basis: 80%;
@@ -70,5 +101,30 @@ const RightWrapper = styled.div`
   align-items: flex-start;
   margin-top: 20px;
 `;
+
+const ReviewSection = styled.div`
+  padding: 20px;
+`;
+
+const ReviewHeader = styled.div``;
+
+const ReviewScore = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  color: ${COLOR.GREY};
+`;
+
+const ReviewBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 10px;
+  padding: 20px 0px;
+  border-bottom: 1px solid ${COLOR.LINE};
+`;
+const StarRating = styled.div``;
+const ReviewCount = styled.div``;
+const ReviewText = styled.div``;
 
 export default DetailPage;

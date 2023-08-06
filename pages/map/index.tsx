ae829c965b8ff827d7973c4f72d2aa18e611b030
rbgksqkr <rbgks1937@naver.com>
2023-08-06 21:59:42 +0900

    Feat: #29 - 마커대로 지도에 선 긋기

--- pages/map/index.tsx
+++ pages/map/index.tsx
@@ -1,8 +1,8 @@
+import COLOR from '@constants/colors';
+import axios from 'axios';
 import type { NextPage } from 'next';
 import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
-import FONT from '../../src/constants/fonts';
-import Loading from '../Loading';
 
 declare global {
   interface Window {
@@ -10,42 +10,31 @@ declare global {
   }
 }
 
+// interface HEADER {
+//   appKey: string | undefined;
+// }
+
+const APP_KEY = process.env.NEXT_PUBLIC_TMAP_KEY;
+const CENTER = { LAT: '37.53084364186228', LNG: '127.081908811749' };
+const START = { LAT: '37.519892712436906', LNG: '127.02810900563199' };
+const END = { LAT: '37.49288934463672', LNG: '127.11971717230388' };
+// const START_LAT = '37.519892712436906';
+// const START_LNG = '127.02810900563199';
+// const END_LAT = '37.49288934463672';
+// const END_LNG = '127.11971717230388';
+const PATH_MARKER_1 = { LAT: '37.5591696189164', LNG: '127.07389565460413' };
+const PATH_MARKER_2 = { LAT: '37.52127761904626', LNG: '127.13346617572014' };
+
+let CURRENT_MAP: any;
 const Index: NextPage = () => {
   const mapRef = useRef<HTMLDivElement | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  // 현재 위치(위도, 경도) 가져온다.
-  // const getPosSuccess = async (pos: GeolocationPosition) => {
-  //   setCurrentCoords({
-  //     lat: pos.coords.latitude,
-  //     lng: pos.coords.longitude
-  //   });
-  //   setIsLoading(false);
-
-  //   if (mapRef.current) mapRef.current.style.display = 'block';
-  // };
-
-  // const getCurrentPosition = async () => {
-  //   navigator.geolocation.getCurrentPosition(
-  //     getPosSuccess,
-  //     () => alert('위치 정보를 가져오는데 실패했습니다.'),
-  //     {
-  //       enableHighAccuracy: false,
-  //       maximumAge: 30000,
-  //       timeout: 27000
-  //     }
-  //   );
-  // };
-
-  // useEffect(() => {
-  //   getCurrentPosition();
-  // }, []);
-
-  const initTmap = () => {
+  const initTmap = async () => {
     // map 생성
     // Tmapv2.Map을 이용하여, 지도가 들어갈 div, 넓이, 높이를 설정합니다.
-    const map = new window.Tmapv2.Map('map_div', {
-      center: new window.Tmapv2.LatLng(37.53084364186228, 127.078908811749), // 지도 초기 좌표
+    CURRENT_MAP = new window.Tmapv2.Map('map_div', {
+      center: new window.Tmapv2.LatLng(CENTER.LAT, CENTER.LNG), // 지도 초기 좌표
       width: '100%',
       height: '400px',
       zoom: 12
@@ -53,48 +42,194 @@ const Index: NextPage = () => {
 
     // 출발
     const startMarker = new window.Tmapv2.Marker({
-      position: new window.Tmapv2.LatLng(37.519892712436906, 127.02810900563199),
+      position: new window.Tmapv2.LatLng(START.LAT, START.LNG),
 
       icon: 'http://tmapapi.sktelecom.com/upload/tmap/marker/pin_r_m_s.png',
       iconSize: new window.Tmapv2.Size(24, 38),
-      map: map
+      map: CURRENT_MAP
     });
 
     // 도착
     const endMarker = new window.Tmapv2.Marker({
-      position: new window.Tmapv2.LatLng(37.49288934463672, 127.11971717230388),
+      position: new window.Tmapv2.LatLng(END.LAT, END.LNG),
 
       icon: 'http://tmapapi.sktelecom.com/upload/tmap/marker/pin_r_m_e.png',
       iconSize: new window.Tmapv2.Size(24, 38),
-      map: map
+      map: CURRENT_MAP
     });
 
     // 경로1
     const pathMarker1 = new window.Tmapv2.Marker({
-      position: new window.Tmapv2.LatLng(37.5591696189164, 127.07389565460413),
+      position: new window.Tmapv2.LatLng(PATH_MARKER_1.LAT, PATH_MARKER_1.LNG),
       icon: 'http://tmapapi.sktelecom.com/upload/tmap/marker/pin_b_m_p.png',
       iconSize: new window.Tmapv2.Size(24, 38),
-      map: map
+      map: CURRENT_MAP
     });
 
     // 경로2
     const pathMarker2 = new window.Tmapv2.Marker({
-      position: new window.Tmapv2.LatLng(37.52127761904626, 127.13346617572014),
+      position: new window.Tmapv2.LatLng(PATH_MARKER_2.LAT, PATH_MARKER_2.LNG),
       icon: 'http://tmapapi.sktelecom.com/upload/tmap/marker/pin_b_m_p.png',
       iconSize: new window.Tmapv2.Size(24, 38),
-      map: map
+      map: CURRENT_MAP
     });
 
-    setIsLoading(false);
+    var polyline = new window.Tmapv2.Polyline({
+      path: [
+        new window.Tmapv2.LatLng(START.LAT, START.LNG),
+        new window.Tmapv2.LatLng(PATH_MARKER_1.LAT, PATH_MARKER_1.LNG),
+        new window.Tmapv2.LatLng(PATH_MARKER_2.LAT, PATH_MARKER_2.LNG),
+        new window.Tmapv2.LatLng(END.LAT, END.LNG)
+      ],
+      strokeColor: COLOR.BLUE1,
+      strokeWeight: 8,
+      map: CURRENT_MAP
+    });
+
+    // const passList = '127.07389565460413,37.5591696189164_127.13346617572014,37.52127761904626';
+    // let prtcl;
+
+    // const requestData = {
+    //   startX: START_LNG,
+    //   startY: START_LAT,
+    //   endX: END_LNG,
+    //   endY: END_LAT,
+    //   passList: passList,
+    //   reqCoordType: 'WGS84GEO',
+    //   resCoordType: 'WGS84GEO',
+    //   startName: '출발지',
+    //   endName: '도착지'
+    // };
+
+    // const headers = {
+    //   appKey: APP_KEY
+    // };
+
+    // await axios
+    //   .post(
+    //     'https://apis.openapi.sk.com/tmap/routes/pedestrian?version=1&format=json&callback=result',
+    //     requestData,
+    //     { headers: headers }
+    //   )
+    //   .then((response) => {
+    //     prtcl = response;
+    //     const resultData = response.data.features;
+    //     const tDistance =
+    //       '총 거리 : ' + (resultData[0].properties.totalDistance / 1000).toFixed(1) + 'km,';
+    //     const tTime = ' 총 시간 : ' + (resultData[0].properties.totalTime / 60).toFixed(0) + '분';
+    //     const resultText = tDistance + tTime;
+    //     console.log(resultData);
+
+    //     // 5. 경로탐색 결과 Line 그리기
+    //     // 교통정보에 따라 달라지는 색깔
+    //     // const trafficColors = {
+    //     //   extractStyles: true,
+    //     //   /* 실제 교통정보가 표출되면 아래와 같은 Color로 Line이 생성됩니다. */
+    //     //   trafficDefaultColor: COLOR.BLUE1, //Default
+    //     //   trafficType1Color: COLOR.GREEN, //원할
+    //     //   trafficType2Color: COLOR.ORANGE, //지체
+    //     //   trafficType3Color: COLOR.RED //정체
+    //     // };
+
+    //     // // 선 스타일
+    //     // const styleRed = {
+    //     //   fillColor: '#FF0000',
+    //     //   fillOpacity: 0.2,
+    //     //   strokeColor: '#FF0000',
+    //     //   strokeWidth: 3,
+    //     //   strokeDashstyle: 'solid',
+    //     //   pointRadius: 2,
+    //     //   title: 'this is a red line'
+    //     // };
+
+    //     const drawInfoArr = [];
+
+    //     for (var i in resultData) {
+    //       //for문 [S]
+    //       var geometry = resultData[i].geometry;
+    //       var properties = resultData[i].properties;
+    //       var polyline_;
+    //       if (geometry.type == 'LineString') {
+    //         for (const j in geometry.coordinates) {
+    //           // 경로들의 결과값(구간)들을 포인트 객체로 변환
+    //           const latlng = new window.Tmapv2.Point(
+    //             geometry.coordinates[j][0],
+    //             geometry.coordinates[j][1]
+    //           );
+    //           // 포인트 객체를 받아 좌표값으로 변환
+    //           const convertPoint = new window.Tmapv2.Projection.convertEPSG3857ToWGS84GEO(latlng);
+    //           // 포인트객체의 정보로 좌표값 변환 객체로 저장
+    //           const convertChange = new window.Tmapv2.LatLng(convertPoint._lat, convertPoint._lng);
+    //           // 배열에 담기
+    //           drawInfoArr.push(convertChange);
+    //         }
+    //       } else {
+    //         var markerImg = '';
+    //         var pType = '';
+    //         var size;
+
+    //         if (properties.pointType == 'S') {
+    //           //출발지 마커
+    //           markerImg = 'http://tmapapi.sktelecom.com/upload/tmap/marker/pin_r_m_s.png';
+    //           pType = 'S';
+    //           size = new window.Tmapv2.Size(24, 38);
+    //         } else if (properties.pointType == 'E') {
+    //           //도착지 마커
+    //           markerImg = 'http://tmapapi.sktelecom.com/upload/tmap/marker/pin_r_m_e.png';
+    //           pType = 'E';
+    //           size = new window.Tmapv2.Size(24, 38);
+    //         } else {
+    //           //각 포인트 마커
+    //           markerImg = 'http://topopen.tmap.co.kr/imgs/point.png';
+    //           pType = 'P';
+    //           size = new window.Tmapv2.Size(8, 8);
+    //         }
+
+    //         // 경로들의 결과값들을 포인트 객체로 변환
+    //         var latlon = new window.Tmapv2.Point(geometry.coordinates[0], geometry.coordinates[1]); // 포인트 객체를 받아 좌표값으로 다시 변환
+    //         var convertPoint = new window.Tmapv2.Projection.convertEPSG3857ToWGS84GEO(latlon);
+
+    //         var routeInfoObj = {
+    //           markerImage: markerImg,
+    //           lng: convertPoint._lng,
+    //           lat: convertPoint._lat,
+    //           pointType: pType
+    //         };
+
+    //         // Marker 추가 (흰색 동그라미 마커)
+    //         const marker_p = new window.Tmapv2.Marker({
+    //           position: new window.Tmapv2.LatLng(routeInfoObj.lat, routeInfoObj.lng),
+    //           icon: routeInfoObj.markerImage,
+    //           iconSize: size,
+    //           map: CURRENT_MAP
+    //         });
+    //       }
+    //     }
+    //     console.log('RESULT: ', drawInfoArr);
+    //     drawLine(drawInfoArr);
+
+    //     function drawLine(arrPoint: any[]) {
+    //       var polyline_;
+
+    //       polyline_ = new window.Tmapv2.Polyline({
+    //         path: arrPoint,
+    //         strokeColor: '#DD0000',
+    //         strokeWeight: 6,
+    //         map: CURRENT_MAP
+    //       });
+    //     }
+    //   })
+    //   .catch((e) => console.log(e));
+    // setIsLoading(false);
   };
 
   useEffect(() => {
     initTmap();
+    // getRoutingResult();
   }, []);
 
   return (
     <>
-      {isLoading && <Loading />}
       <div ref={mapRef} id='map_div'></div>
     </>
   );

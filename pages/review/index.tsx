9754336be5a7a653e34e8e250e282d29da915ab8
rbgksqkr <rbgks1937@naver.com>
2023-08-07 17:18:44 +0900

    Feat: #34 - 텍스트 부분 퍼블리싱

--- pages/review/index.tsx
+++ pages/review/index.tsx
@@ -1,5 +1,6 @@
 import Header from '@common/Header';
 import COLOR from '@constants/colors';
+import FONT from '@constants/fonts';
 import { NextPage } from 'next';
 import { styled } from 'styled-components';
 
@@ -7,6 +8,31 @@ const ReviewPage: NextPage = () => {
   return (
     <ReviewMainWrapper>
       <Header />
+      <div
+        style={{
+          height: '85vh',
+          backgroundColor: 'red',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: 20
+        }}
+      >
+        <ReviewTitle style={FONT.HEADLINE1}>이용한 경로에 대한 평가를 부탁드립니다.</ReviewTitle>
+        <div style={FONT.HEADLINE1}>{'⭐️'.repeat(5)}</div>
+        <textarea
+          style={{
+            width: '90%',
+            height: '15%',
+            fontSize: 16,
+            padding: 10,
+            outline: 'none',
+            resize: 'none'
+          }}
+          placeholder='자유롭게 의견을 적어주세요.'
+        />
+      </div>
     </ReviewMainWrapper>
   );
 };
@@ -14,6 +40,14 @@ const ReviewPage: NextPage = () => {
 const ReviewMainWrapper = styled.div`
   border: 1px solid black;
   height: 100%;
+`;
+
+const ReviewTitle = styled.div`
+  width: 70%;
+  display: flex;
+  text-align: center;
+  margin: 0 auto;
+  fiex-wrap: wrap;
 `;
 
 export default ReviewPage;

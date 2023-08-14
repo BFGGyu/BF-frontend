89c2a5b6c419e2d905b88fe2fe98eb1cb73f1d1b
rbgksqkr <rbgks1937@naver.com>
2023-08-14 16:40:48 +0900

    Feat: #64 - 카카오 로그인 연결

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,11 +1,18 @@
 import Header from '@common/Header';
 import FONT from '@constants/fonts';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import React from 'react';
 import styled from 'styled-components';
 
 const LoginPage = () => {
-  const handleClickLogin = () => {};
+  const router = useRouter();
+
+  const handleClickLogin = () => {
+    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_APP_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code`;
+    router.push(KAKAO_AUTH_URL);
+  };
+
   return (
     <LoginWrapper>
       <Header page='review' />
